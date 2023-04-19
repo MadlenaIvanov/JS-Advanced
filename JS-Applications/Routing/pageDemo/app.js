679efaacea20c76f3df9447f456811b8a479f4a4
Madlena Ivanova <madlena.iv@abv.bg>
2023-04-19 13:22:49 +0300

    routing

--- JS-Applications/Routing/pageDemo/app.js
+++ JS-Applications/Routing/pageDemo/app.js
@@ -1,1 +1,23 @@
-console.log('loaded external sxript')
+import page from '//unpkg.com/page/page.mjs';
+
+const pages = {
+    '/home': '<h2>Home page</h2><p>Home page content</p>',
+    '/catalog': '<h2>Catalog</h2><p>List of acticles</p>',
+    '/catalog/123': '<h2>Item 123</h2><p>Item details</p>',
+    '/about': '<h2>About Us</h2><p>Contact information</p>'
+};
+
+const defaultPage = '<h2>404</h2><p>Page Not Found</p>';
+
+const main = document.querySelector('main');
+
+page('/home', () => updateContent('/home'));
+page('/catalog', () => updateContent('/catalog'));
+page('/catalog/123', () => updateContent('/catalog/123'));
+page('/about', () => updateContent('/about'));
+page.start();
+
+
+function updateContent(pathname) {
+    main.innerHTML = pages[pathname] || defaultPage;
+}
